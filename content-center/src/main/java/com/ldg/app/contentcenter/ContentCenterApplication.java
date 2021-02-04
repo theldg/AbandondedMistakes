d5e29455df916605b464d6cb7e63a17894327f0f
theldg <714838871@qq.com>
2021-02-04 18:21:50 +0800

    content-center更新
    Signed-off-by: theldg <714838871@qq.com>

--- content-center/src/main/java/com/ldg/app/contentcenter/ContentCenterApplication.java
+++ content-center/src/main/java/com/ldg/app/contentcenter/ContentCenterApplication.java
@@ -1,13 +1,11 @@
 package com.ldg.app.contentcenter;
 
 import com.ldg.app.contentcenter.configuration.feign.UserCenterFeignConfiguration;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
